d667a404a0e9ef5dc064cde899c05cd697d1a8c3
SimplySaid <SimplySaid@users.noreply.github.com>
2021-05-18 17:07:17 -0400

    Fixed extractlist bug
    extract list was reading from CSV which was being truncated by seperator. Switched over to taking XLSX extract list as input.

--- input/WorldBank/extractconfig.py
+++ input/WorldBank/extractconfig.py
@@ -1,6 +1,6 @@
 #File Locations
 
-EXTRACT_LIST_FILE = "ExtractList.csv"
+EXTRACT_LIST_FILE = "ExtractList.xlsx"
 RAW_DATA_FILE = "../../output/WorldBank/WDIData.csv"
 OUTPUT_PATH = "../../output/WorldBank/split_output/"
 
--- input/WorldBank/wbextracter.py
+++ input/WorldBank/wbextracter.py
@@ -4,18 +4,23 @@ import numpy as np
 import os
 import extractconfig
 
+
+
 #Read Raw Data into Pandas Dataframe
 extract_file_path = os.path.join(os.path.dirname(__file__), extractconfig.RAW_DATA_FILE)
 raw_data = pd.read_csv(extract_file_path)
 
 #Read ExtractList into List
 extract_list_path = os.path.join(os.path.dirname(__file__),extractconfig.EXTRACT_LIST_FILE)
-extract_list = np.genfromtxt(extract_list_path, delimiter = ';' , dtype = str)
-extract_list = extract_list[:].tolist()
+# extract_list = np.genfromtxt(extract_list_path, delimiter = ',' , dtype = str)
+# extract_list = extract_list[:].tolist()
+extract_list_path = os.path.join(os.path.dirname(__file__),extractconfig.EXTRACT_LIST_FILE)
+extract_list = pd.read_excel(extract_list_path, engine='openpyxl')
+extract_list = extract_list['Indicator Name'].tolist()
 
 #Create Pivot for each file in ExtractList.xlsx
 for e in extract_list:
-    if (not(e in raw_data['Indicator Name'].values)):
+    if (not(e.strip() in raw_data['Indicator Name'].values)):
         print (e + "does not exist in raw data file")
     else:
         temp_df = raw_data.loc[raw_data['Indicator Name'] == e]
@@ -23,8 +28,4 @@ for e in extract_list:
 
         output_path = os.path.join(os.path.dirname(__file__),extractconfig.OUTPUT_PATH + e + ".xlsx")
         temp_df.to_excel(output_path)
-
-
-
-
 
