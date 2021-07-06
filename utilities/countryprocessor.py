8c9859edd6238273353977ea7489d659f45dd6b5
SimplySaid <SimplySaid@users.noreply.github.com>
2021-07-05 21:34:14 -0400

    Added WHO Datastream

--- utilities/countryprocessor.py
+++ utilities/countryprocessor.py
@@ -4,17 +4,15 @@ import os
 import country_converter as coco
 from pathlib import Path
 
-def countryprocessor (df, country_col_name):
-    #Adds the Index. Index is used so AWS Glue can process headers
-    if 'Index' in df:
-        del df['Index']
-    df.index.name = "Index"
-    
+def correct_country (df, country_col_name):    
     #Standardizes Country Name using Country Converter
     df[country_col_name] = df[country_col_name].apply(lambda x: coco.convert(names = x, to='name_short', not_found = None))
 
+    return df
+
+
+def add_iso_code (df, country_col_name):
     #Adds ISO-3166 3 Digit Country Code - This will be used to join with other data sets
     df['Country Code'] = df[country_col_name].apply(lambda x: coco.convert(names = x, to = 'ISO3', not_found = None))
 
     return df
-
