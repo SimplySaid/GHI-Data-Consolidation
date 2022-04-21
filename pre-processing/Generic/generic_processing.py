555fa5d2f687829358699742bbeafd631beef8d4
SimplySaid <SimplySaid@users.noreply.github.com>
2022-04-20 23:51:18 -0400

    Fixed generic processing
    Started working on citations w/ User Input
    Fixed bugs
    
    To do: Add options to process columns, years, etc

--- pre-processing/Generic/generic_processing.py
+++ pre-processing/Generic/generic_processing.py
@@ -18,6 +18,15 @@ import numpy as np
 import csv
 from glob import glob
 import os
+import cmd
+
+def get_citations():
+    source_name = input("What is the source name? (e.g., The World Bank: DataBank [Internet]. Washington D.C: The World Bank Group")
+    source_download_link = input("What is the source link")
+    source_year = input("What year do you want to extract? Enter 9999 for most recent available year by country")
+    source = "Reference:%s;%s \n\nSource Link:%s" % (source_name, source_year, source_download_link)
+
+    return source
 
 
 def process_generic_data(year = None, year_consolidation = False):
@@ -27,11 +36,15 @@ def process_generic_data(year = None, year_consolidation = False):
     filenames = glob(data_path + "\[!~]*.xlsx") + glob(data_path + "\[!~]*.csv") + glob(data_path + "\[!~]*.xls")
 
     for filename in filenames:
-        
+
+        # Generate Inputs
+        print("Processing" + " " + filename + ":")
+        # citation = get_citations()
+
         ext = os.path.splitext(filename)[1]
 
         if ext == '.csv':
-            file = pd.read_csv(filename)
+            file = pd.read_csv(filename, encoding='latin-1')
         elif ext == '.xls':
             file = pd.read_excel(filename)
         elif ext == '.xlsx':
@@ -45,8 +58,11 @@ def process_generic_data(year = None, year_consolidation = False):
 
         file_headers = list(file)
         if 'location_name' in file_headers:
-            file['location_name'] = file['location_name'].apply(lambda x: c.convert(names = x, to='name_short', not_found = None))
-            file['iso3_location'] = file['location_name'].apply(lambda x: c.convert(names = x, to = 'ISO3', not_found = None))
+            try: 
+                file['location_name'] = file['location_name'].apply(lambda x: c.convert(names = x, to='name_short', not_found = None))
+                file['iso3_location'] = file['location_name'].apply(lambda x: c.convert(names = x, to = 'ISO3', not_found = None))
+            except:
+                print("Error Occured: Check the file for footers")
         elif 'iso3_location' in file_headers:
             file['location'] = file['iso3_location'].apply(lambda x: c.convert(names = x, to='name_short', not_found = None))
             file['iso3_location'] = file['iso3_location'].apply(lambda x: c.convert(names = x, to = 'ISO3', not_found = None))
